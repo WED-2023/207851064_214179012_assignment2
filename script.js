1ced334027665ab2d2eacbcde348d7a248c6e6bb
naveh100innituch <navehg@post.bgu.ac.il>
2025-04-21 16:09:16 +0300

    Create script.js

---
+++ script.js
@@ -0,0 +1,5 @@
+function navigate(screenId) {
+  const screens = document.querySelectorAll(".screen");
+  screens.forEach(screen => screen.classList.remove("active"));
+  document.getElementById(screenId).classList.add("active");
+}
